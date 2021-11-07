bf5907ae021fff9e2580afaf77633dba0203d5c5
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2021-11-07 21:54:12 +0000

    Adding loading indicator

--- apps/documentation/src/app/app.component.ts
+++ apps/documentation/src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  loading$ = new BehaviorSubject<boolean>(false);
+
+  constructor(private readonly router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.loading$.next(true);
+      }
+      if (event instanceof NavigationEnd) {
+        this.loading$.next(false);
+      }
+    });
+  }
+}
