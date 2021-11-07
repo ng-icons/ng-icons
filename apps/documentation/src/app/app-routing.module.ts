e6ab17cb4e539d36c998de454b1d3d73f6fc2d06
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2021-11-07 23:07:57 +0000

    Update app-routing.module.ts

--- apps/documentation/src/app/app-routing.module.ts
+++ apps/documentation/src/app/app-routing.module.ts
@@ -44,7 +44,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
