45f96426f07cad2e9dfa39aeb8a4b03059aad989
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2022-08-31 00:06:07 +0100

    reverting

--- tools/generators/icon-library/utils/add-deploy-target.ts
+++ tools/generators/icon-library/utils/add-deploy-target.ts
@@ -8,12 +8,11 @@ import { Schema } from '../schema';
 export function addDeployTarget(tree: Tree, schema: Schema) {
   const configuration = readProjectConfiguration(tree, schema.name);
 
-  configuration.targets!.publish = {
-    executor: '@nrwl/workspace:run-commands',
+  configuration.targets!.deploy = {
+    executor: 'ngx-deploy-npm:deploy',
     options: {
-      command: `node tools/scripts/publish.mjs ${schema.name} {args.ver} {args.tag}`,
+      access: 'public',
     },
-    dependsOn: ['build'],
   };
 
   updateProjectConfiguration(tree, schema.name, configuration);
