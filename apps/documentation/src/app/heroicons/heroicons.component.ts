025869dd73355e6d2302e36267ab88fe7db82ea7
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2022-09-01 22:18:25 +0100

    feat: heroicons mini icons (#35)

--- apps/documentation/src/app/heroicons/heroicons.component.ts
+++ apps/documentation/src/app/heroicons/heroicons.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import * as miniIcons from '@ng-icons/heroicons/mini';
 import * as outlineIcons from '@ng-icons/heroicons/outline';
 import * as solidIcons from '@ng-icons/heroicons/solid';
 
 @Component({
   selector: 'app-heroicons',
   templateUrl: './heroicons.component.html',
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-direction: column;
+        gap: 3rem;
+      }
+    `,
+  ],
 })
 export class HeroiconsComponent {
   outlineIcons = outlineIcons;
   solidIcons = solidIcons;
+  miniIcons = miniIcons;
 }
---
+++ packages/heroicons/mini/README.md
@@ -0,0 +1,3 @@
+# @ng-icons/heroicons/mini
+
+Secondary entry point of `@ng-icons/heroicons`. It can be used by importing from `@ng-icons/heroicons/mini`.
