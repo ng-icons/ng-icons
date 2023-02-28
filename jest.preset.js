77015abcd57088b99dd130d297fffa3803923236
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2023-02-28 22:35:47 +0000

    updating nx

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,5 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+};
--- packages/dripicons/jest.config.ts
+++ packages/dripicons/jest.config.ts
@@ -3,15 +3,16 @@ export default {
   displayName: 'dripicons',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: '../../coverage/packages/dripicons',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
