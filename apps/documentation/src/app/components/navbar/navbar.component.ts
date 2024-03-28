15538139e68f3de5936becbfb377a5929a2cab54
Ashley Hunter <ahunter2@opentext.com>
2024-03-28 23:21:58 +0000

    updating docs

--- apps/documentation/src/app/components/navbar/navbar.component.ts
+++ apps/documentation/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { bootstrapGithub } from '@ng-icons/bootstrap-icons';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
@@ -7,13 +8,12 @@ import { fromEvent } from 'rxjs';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgIconComponent, RouterLink, RouterLink],
+  imports: [NgIconComponent, RouterLink, RouterLink, NgClass],
   providers: [provideIcons({ bootstrapGithub })],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  @HostBinding('class.blurred')
   showBlurredNavbar = false;
 
   ngOnInit(): void {
