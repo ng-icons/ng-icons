70a9859eacb9bd4c63019f84048e29e097e43147
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2024-07-25 23:06:44 +0100

    docs: adding ng-primitives abnner

--- apps/documentation/tailwind.config.js
+++ apps/documentation/tailwind.config.js
@@ -1,8 +1,8 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
@@ -12,6 +12,9 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['eudoxus sans', 'sans-serif'],
+      },
+      boxShadow: {
+        promo: 'inset 0 -2px 0 #dc9594',
       },
     },
   },
