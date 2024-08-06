7af2ebb4e9d69317913b828f509c461f72392f3b
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2024-08-06 08:45:54 +0000

    fixing tests

--- packages/devicon/src/index.spec.ts
+++ packages/devicon/src/index.spec.ts
@@ -1,7 +1,11 @@
-import * as icons from './index';
+import * as lineIcons from '../line/src/index';
+import * as plainIcons from '../plain/src/index';
+import * as originalIcons from '../original/src/index';
 
 describe('Devicon Icons', () => {
   it('should ensure the export names have not changed unexpectedly', () => {
-    expect(Object.keys(icons)).toMatchSnapshot();
+    expect(Object.keys(lineIcons)).toMatchSnapshot();
+    expect(Object.keys(plainIcons)).toMatchSnapshot();
+    expect(Object.keys(originalIcons)).toMatchSnapshot();
   });
 });
