0ddf6067205ca7eb23eb958e275f890775b08b31
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2024-11-09 21:42:49 +0000

    stack

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,8 @@ export * from './lib/components/glyph/glyph.component';
 export * from './lib/components/icon/icon-name';
 export * from './lib/components/icon/icon.component';
 export * from './lib/icon.module';
+export { withContentSecurityPolicy } from './lib/providers/features/csp';
+export { withExceptionLogger } from './lib/providers/features/logger';
 export * from './lib/providers/glyph-config.provider';
 export {
   NgGlyphs,
@@ -11,8 +13,8 @@ export {
 export * from './lib/providers/icon-config.provider';
 export * from './lib/providers/icon-loader.provider';
 export * from './lib/providers/icon.provider';
-export { withContentSecurityPolicy } from './lib/providers/features/csp';
-export { withExceptionLogger } from './lib/providers/features/logger';
 
 // re-export the component as NgIconComponent to prevent breaking changes
 export { NgIcon as NgIconComponent } from './lib/components/icon/icon.component';
+
+export * from './lib/components/icon-stack/icon-stack.component';
