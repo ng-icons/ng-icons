0fa13b3d5241446b4d18707b2a50b87bab490571
abiramcodes <codingkannan@gmail.com>
2025-10-26 14:23:39 +0530

    refactor: removed ngClass +  navbar template to control flow

--- apps/documentation/src/app/components/navbar/navbar.component.ts
+++ apps/documentation/src/app/components/navbar/navbar.component.ts
@@ -1,28 +1,29 @@
-import { NgClass } from '@angular/common';
 import { Component, Signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterLink } from '@angular/router';
 import { bootstrapGithub } from '@ng-icons/bootstrap-icons';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { distinctUntilChanged, fromEvent, map } from 'rxjs';
+import { NAV_LINKS } from '../../constants/nav-link.constant';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgIconComponent, RouterLink, RouterLink, NgClass],
+  imports: [NgIconComponent, RouterLink],
   providers: [provideIcons({ bootstrapGithub })],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  public readonly showBlurredNavbar: Signal<boolean>;
+  protected readonly showBlurredNavbar: Signal<string>;
+  protected readonly navLinks = NAV_LINKS;
 
   constructor() {
     this.showBlurredNavbar = toSignal(
       fromEvent(window, 'scroll').pipe(
-        map(() => window.scrollY > 0),
+        map(() => (window.scrollY > 0 ? 'bg-black/5 backdrop-blur-sm' : '')),
         distinctUntilChanged(),
       ),
-      { initialValue: false },
+      { initialValue: '' },
     );
   }
 }
