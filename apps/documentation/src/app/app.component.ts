d851f1246e9a4fc71c0aad824885650c7b7c6aea
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2025-12-05 10:07:32 +0000

    Update app.component.ts

--- apps/documentation/src/app/app.component.ts
+++ apps/documentation/src/app/app.component.ts
@@ -22,16 +22,10 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        switch (event.urlAfterRedirects) {
-          case '/':
-            this.background = 'red';
-            break;
-          case '/getting-started':
-            this.background = 'blue';
-            break;
-          case '/browse-icons':
-            this.background = 'blue';
-            break;
+        if (event.urlAfterRedirects === '/') {
+          this.background = 'red';
+        } else {
+          this.background = 'blue';
         }
       }
     });
