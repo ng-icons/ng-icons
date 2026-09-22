15627874a149955ef60dc7afcd38279970258b6a
apieper <anton.pieper@nethinks.com>
2026-09-22 13:57:47 +0200

    docs(font-awesome): follow existing icon upgrade conventions

--- tools/workspace-plugin/src/generators/svg-to-ts/iconsets.ts
+++ tools/workspace-plugin/src/generators/svg-to-ts/iconsets.ts
@@ -284,7 +284,7 @@ export const iconsets: Iconset[] = [
   {
     glob: '*.svg',
     gitRepo: 'https://github.com/FortAwesome/Font-Awesome.git',
-    gitRef: '14c65a3747d0f3b751f15831fc719236aea8729d', // Font Awesome Free 7.3.1
+    gitRef: '14c65a3747d0f3b751f15831fc719236aea8729d',
     gitPath: 'svgs/solid',
     output: 'packages/font-awesome/solid/src/index.ts',
     getIconName: (name: string) => `faSolid${name}`,
@@ -295,7 +295,7 @@ export const iconsets: Iconset[] = [
   {
     glob: '*.svg',
     gitRepo: 'https://github.com/FortAwesome/Font-Awesome.git',
-    gitRef: '14c65a3747d0f3b751f15831fc719236aea8729d', // Font Awesome Free 7.3.1
+    gitRef: '14c65a3747d0f3b751f15831fc719236aea8729d',
     gitPath: 'svgs/regular',
     output: 'packages/font-awesome/regular/src/index.ts',
     getIconName: (name: string) => `fa${name}`,
@@ -306,7 +306,7 @@ export const iconsets: Iconset[] = [
   {
     glob: '*.svg',
     gitRepo: 'https://github.com/FortAwesome/Font-Awesome.git',
-    gitRef: '14c65a3747d0f3b751f15831fc719236aea8729d', // Font Awesome Free 7.3.1
+    gitRef: '14c65a3747d0f3b751f15831fc719236aea8729d',
     gitPath: 'svgs/brands',
     output: 'packages/font-awesome/brands/src/index.ts',
     getIconName: (name: string) => `faBrand${name}`,
